d0967e0b8a2653ce5e57ae23947c32dec14bcc8c
Adrien Rosi <adrienrosi@gmail.com>
2025-05-11 12:30:52 +0200

    fix: exports

---
+++ src/dpe-audit.ts
@@ -0,0 +1,29 @@
+import {
+  createAudit,
+  replaceAudit,
+  updateAudit,
+  searchAudits,
+  retrieveAudit
+} from './audit'
+import { retrieveChauffage } from './chauffage'
+import { retrieveEclairage } from './eclairage'
+import { retrieveEcs } from './ecs'
+import { retrieveEnveloppe } from './enveloppe'
+import { retrieveRefroidissement } from './refroidissement'
+import { retrieveVentilation } from './ventilation'
+
+export const DPEAudit = {
+  audit: {
+    retrieveAudit,
+    createAudit,
+    replaceAudit,
+    updateAudit,
+    searchAudits
+  },
+  chauffage: { retrieveChauffage },
+  eclairage: { retrieveEclairage },
+  ecs: { retrieveEcs },
+  enveloppe: { retrieveEnveloppe },
+  refroidissement: { retrieveRefroidissement },
+  ventilation: { retrieveVentilation }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-import {
-  createAudit,
-  replaceAudit,
-  updateAudit,
-  searchAudits,
-  retrieveAudit
-} from './audit'
-import { retrieveChauffage } from './chauffage'
-import { retrieveEclairage } from './eclairage'
-import { retrieveEcs } from './ecs'
-import { retrieveEnveloppe } from './enveloppe'
-import { retrieveRefroidissement } from './refroidissement'
-import { retrieveVentilation } from './ventilation'
-
-const DPEAudit = {
-  audit: {
-    retrieveAudit,
-    createAudit,
-    replaceAudit,
-    updateAudit,
-    searchAudits
-  },
-  chauffage: { retrieveChauffage },
-  eclairage: { retrieveEclairage },
-  ecs: { retrieveEcs },
-  enveloppe: { retrieveEnveloppe },
-  refroidissement: { retrieveRefroidissement },
-  ventilation: { retrieveVentilation }
-}
-
-export default DPEAudit
+export { DPEAudit } from './dpe-audit'
+export * as Audit from './audit'
+export * as Chauffage from './chauffage'
+export * as Eclairage from './eclairage'
+export * as Ecs from './ecs'
+export * as Enveloppe from './enveloppe'
+export * as Refroidissement from './refroidissement'
+export * as Ventilation from './ventilation'
